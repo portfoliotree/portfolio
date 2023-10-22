ed14ecd965b90438f97400512e01844afb607ac6
Christopher Hunter <8398225+crhntr@users.noreply.github.com>
2023-10-21 20:15:46 -0700

    simplify implementation of AddColumn

--- returns/table.go
+++ returns/table.go
@@ -196,39 +196,23 @@ func (table Table) addAdditionalColumn(list List) Table {
 	list = list.Between(table.LastTime(), table.FirstTime())
 	updated := table.Between(list.LastTime(), list.FirstTime())
 
+	newValues := make([]float64, len(updated.times))
 	for _, r := range list {
-		_, updated = updated.ensureRowForTime(r.Time)
+		i, found := updated.rowForTime(r.Time)
+		if !found {
+			continue
+		}
+		newValues[i] = r.Value
 	}
 
-	newValues := make([]float64, len(updated.times))
-	for i, tm := range updated.times {
-		value, _ := list.Value(tm)
-		newValues[i] = value
-	}
 	updated.values = append(updated.values, newValues)
 	return updated
 }
 
-func (table Table) ensureRowForTime(tm time.Time) (index int, updated Table) {
-	for i, et := range table.times {
-		if et.Equal(tm) {
-			return i, table
-		}
-		if tm.After(et) {
-			index, updated = i, table
-			updated.times = append(updated.times[:i], append([]time.Time{tm}, updated.times[i:]...)...)
-			for j, values := range updated.values {
-				updated.values[j] = append(values[:i], append([]float64{0}, values[i:]...)...)
-			}
-			break
-
-			//// an early return makes the coverage dip below 100% because the
-			//// empty block outside the loop would never execute. This break
-			//// is essentially like the following line
-			// return index, updated
-		}
-	}
-	return index, updated
+func (table Table) rowForTime(tm time.Time) (index int, exists bool) {
+	return slices.BinarySearchFunc(table.times, tm, func(et time.Time, t time.Time) int {
+		return et.Compare(t) * -1
+	})
 }
 
 func (table Table) FirstTime() time.Time { return indexOrEmpty(table.times, firstIndex(table.times)) }
